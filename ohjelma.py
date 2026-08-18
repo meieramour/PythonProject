0abab4db270953857ae4fdbfa766b59cf22b7dcc
meieramour <winterwijzreich@gmail.com>
2026-08-18 09:58:49 +0300

    Initial commit

---
+++ ohjelma.py
@@ -0,0 +1,12 @@
+print('moi maailma')
+print('ohjelmointi on mielenkintoista')
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def sum(a, b):
+    return (a + b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')
